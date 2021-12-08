e0fb8f2e777622d712a89382ec96a8b3f30de471
changxiaoyang <changxiaoyang@cestc.cn>
2021-12-08 15:12:21 +0800

    优化页面

--- web/src/components/MySQL.vue
+++ web/src/components/MySQL.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
     <el-row>
-      <el-select v-model="database" no-data-text="没有数据库">
-        <el-option v-for="database in databases" :label="database.name" :value="database.name" :key="database.id"></el-option>
-      </el-select>
-      <el-button icon="el-icon-video-play" type="success">运行</el-button>
-      <el-button icon="el-icon-video-pause" type="danger">停止</el-button>
+      <el-form :inline="true">
+        <el-form-item label="数据库">
+          <el-select v-model="database" no-data-text="没有数据库" size="small">
+            <el-option v-for="database in databases" :label="database.name" :value="database.name" :key="database.id"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button icon="el-icon-video-play" type="success" size="small" round>运行</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button icon="el-icon-video-pause" type="danger" size="small" round>停止</el-button>
+        </el-form-item>
+      </el-form>
     </el-row>
-    <el-input v-model="command" type="textarea"></el-input>
-    <el-tabs v-model="activeTab" type="border-card">
-      <el-tab-pane label="信息" name="info"> xxxx </el-tab-pane>
+    <el-input v-model="command" type="textarea" :rows="10" style="margin-top: 10px;"></el-input>
+    <el-tabs v-model="activeTab" type="border-card" style="margin-top: 10px;">
       <el-tab-pane label="结果" name="result">
-        <el-table></el-table>
-      </el-tab-pane>
-      <el-tab-pane label="刨析" name="analysis">
-        <el-table></el-table>
-      </el-tab-pane>
-      <el-tab-pane label="状态" name="status">
         <el-table></el-table>
       </el-tab-pane>
     </el-tabs>
@@ -30,10 +31,13 @@ export default {
       command: "",
       database: {},
       databases: [],
-      activeTab: "info",
+      activeTab: "result",
+
     };
   },
 };
 </script>
 
-<style></style>
+<style>
+.el-form-item {margin-bottom: 0;}
+</style>
